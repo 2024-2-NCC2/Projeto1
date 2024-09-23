90b80c69bc532c6c31169af042d07a374cd2e243
Saulo Pereira Jesus <saulopereiradejesus@gmail.com>
2024-09-23 13:56:04 -0300

    feat: dynamic year in footer

--- client/src/components/Fotter.jsx
+++ client/src/components/Fotter.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
+
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+
   return (
     <footer>
       <div className="footer-container">
         <div className="footer-header">
-          <h1 id="foodpath">FoodPath - 2024</h1>
+          <h1 id="foodpath">FoodPath - {year}</h1>
           <p>
             O FoodPath é uma iniciativa dedicada a conscientizar e mobilizar ações para combater a fome no Brasil, <br/> 
             conectando pessoas a ONGs comprometidas com essa causa.
@@ -18,13 +22,13 @@ const Footer = () => {
             <h2 id="doacoes">Doações</h2>
             <p>
               Sua contribuição ajuda a transformar vidas. <br/>
-              <Link to="/donation" className="underline">Clique aqui para doar</Link> e fazer parte dessa causa.
+              <Link to="/donation" className="underline">Clique aqui para doar</Link> <br/> faça parte dessa causa!
             </p>
           </div>
           <div className="footer-section contact">
             <h2 id="contato">Contato</h2>
             <p>
-              Tem dúvidas ou sugestões? <br/> Entre em contato conosco: 
+              Tem dúvidas ou sugestões? <br/> Entre em contato conosco: <br/>
               <a href="mailto:foodpath@gmail.com" className="underline"> foodpath@gmail.com</a>
             </p>
           </div>

6f4c529632cfb7728cf9d423f49b3777036a1e4d
Saulo Pereira Jesus <saulopereiradejesus@gmail.com>
2024-09-24 16:00:15 -0300

    feat: add responsive carousel

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Carousel from '../components/Carousel'
 
 const Home = () => {
   return (
     <div>
+        <Carousel/>
         <h1>Pagina Home</h1>
     </div>
   )

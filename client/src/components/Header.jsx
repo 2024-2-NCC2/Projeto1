f1874b2543296e988d6e478eea2aec881b067f06
Saulo Pereira Jesus <saulopereiradejesus@gmail.com>
2024-09-25 17:06:55 -0300

    fix: navbar redesign

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,17 +1,9 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import NavBar from './NavBar'
 
 const Header = () => {
   return (
-    <header className='app-header'>
-        <div className="foodpath-logo">
-            <Link to="/">
-                <h1>FoodPath</h1>        
-            </Link>
-        </div>
-        <NavBar/>
-    </header>
+    <NavBar/>
   )
 }
 
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <nav className='app-navbar'>
-        <ul>
-            <li><Link to="/aboutus" className='underline'>Quem Somos</Link></li>
-            <li><Link to="/donation" className='underline'>Faça uma Doação</Link></li>
-        </ul>
+    <nav className="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          <img alt="Logo" width="30" height="24" className="d-inline-block align-text-top" />
+          FoodPath
+        </Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link underline" to="/aboutus">Quem Somos</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link underline" to="/donation">Faça uma Doação</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link underline" to="/contact">Contato</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;

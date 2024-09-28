b800fef0d73e4a8b696b26cf6fd1169cc41bd534
Saulo Pereira Jesus <saulopereiradejesus@gmail.com>
2024-09-28 03:19:19 -0300

    fix: improved footer structure and organization

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -9,29 +9,29 @@ const Footer = () => {
   return (
     <footer>
       <div className="footer-container">
-        <div className="footer-header">
+        <div className="footer-left">
           <h1 id="foodpath">FoodPath - {year}</h1>
           <p>
-            O FoodPath é uma iniciativa dedicada a conscientizar e mobilizar ações para combater a fome no Brasil, <br/> 
-            conectando pessoas a ONGs comprometidas com essa causa.
+            O FoodPath é uma plataforma dedicada a mobilizar doações <br/>
+            para combater a fome no Brasil.
           </p>
-          <h1 id="fecap">Fundação Escola de Comércio Álvares Penteado - FECAP</h1>
+          <p><br/>
+          Fundação Escola de Comércio Álvares Penteado - <b>FECAP</b></p>
         </div>
-        <div className="footer-content">
-          <div className="footer-section donations">
-            <h2 id="doacoes">Doações</h2>
-            <p>
-              Sua contribuição ajuda a transformar vidas. <br/>
-              <Link to="/donation" className="underline">Clique aqui para doar</Link> <br/> faça parte dessa causa!
-            </p>
-          </div>
-          <div className="footer-section contact">
-            <h2 id="contato">Contato</h2>
-            <p>
-              Tem dúvidas ou sugestões? <br/> Entre em contato conosco: <br/>
-              <a href="mailto:foodpath@gmail.com" className="underline"> foodpathcontact@gmail.com</a>
-            </p>
-          </div>
+
+        <div className="footer-right">
+          <h2 id="doacoes">Doações</h2>
+          <p>
+            Sua contribuição ajuda a transformar vidas. <br/>
+            <Link to="/donation" className="underline">Clique aqui para doar</Link> <br/>
+            e faça parte dessa causa!
+          </p>
+          <h2 id="contato">Contato</h2>
+          <p>
+            Tem dúvidas ou sugestões? <br/>
+            Entre em contato conosco: <br/>
+            <a href="mailto:foodpathcontact@gmail.com" className="underline">foodpathcontact@gmail.com</a>
+          </p>
         </div>
       </div>
     </footer>

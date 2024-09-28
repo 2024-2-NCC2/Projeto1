e4393d8d5b3a03afd445a705c4b5f6d9a1f78678
Saulo Pereira Jesus <saulopereiradejesus@gmail.com>
2024-09-28 00:57:24 -0300

    fix: rename footer import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import Header from './components/Header';
-import Fotter from './components/Footer';
+import Footer from './components/Footer';
 
 import { Outlet } from 'react-router-dom'
 
@@ -8,7 +8,7 @@ function App() {
     <div className='App'>
       <Header/>
       <Outlet/>
-      <Fotter/>
+      <Footer/>
     </div>
   )
 }

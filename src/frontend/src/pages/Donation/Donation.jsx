91f7ddf694756d281b2555ed4fd95c656bbe9bbf
Saulo Pereira Jesus <saulopereiradejesus@gmail.com>
2024-10-12 20:15:27 -0300

    feat: 2 new ONG cards

--- src/frontend/src/pages/Donation/Donation.jsx
+++ src/frontend/src/pages/Donation/Donation.jsx
@@ -6,6 +6,8 @@ import bancoAlimentosImg from '../../public/assets/images/ongs/ong-banco-aliment
 import misturaiImg from '../../public/assets/images/ongs/ong-misturai-card.png';
 import stopHungerImg from '../../public/assets/images/ongs/ong-stop-hunger-card.png';
 import acaoCidadaniaImg from '../../public/assets/images/ongs/ong-acao-cidadania-card.png';
+import sescImg from '../../public/assets/images/ongs/ong-sesc-card.png';
+import caritasBrasilImg from '../../public/assets/images/ongs/ong-caritas-brasileira-card.png';
 
 const Donation = () => {
   return (
@@ -39,6 +41,18 @@ const Donation = () => {
           description="A Ação da Cidadania trabalha para garantir que todos tenham acesso à alimentação adequada, unindo esforços em ações práticas e políticas. Eles possuem compromisso em transformar vidas, construindo um Brasil onde ninguém passe fome."
           link="https://www.acaodacidadania.org.br/como-apoiar"
           imageSrc={acaoCidadaniaImg}
+        />
+        <Card 
+          title="Sesc - Mesa Brasil"
+          description="A Sesc Mesa Brasil é a maior rede privada de bancos de alimentos da América Latina, combatendo a fome e o desperdício, a partir de doações de parceiros, e contribuindo para a garantia da segurança alimentar e nutricional em todo o Brasil."
+          link="https://www.sesc.com.br/atuacoes/assistencia/sesc-mesa-brasil/participe/"
+          imageSrc={sescImg}
+        />
+        <Card 
+          title="Cáritas Brasileira - Sumaúma"
+          description="As atividades são realizadas na cidade de Boa Vista, e o projeto tem como objetivo promover o acesso a refeições preparadas que atendam às necessidades e preferências nutricionais especializadas de grupos vulneráveis, como gestantes e bebês."
+          link="https://caritas.org.br/colabore"
+          imageSrc={caritasBrasilImg}
         />
       </div>
     </div>

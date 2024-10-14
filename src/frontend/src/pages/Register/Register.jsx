75786a2963fb3945bed463d0d4f3972341e5564a
Saulo Pereira Jesus <saulopereiradejesus@gmail.com>
2024-10-14 15:28:59 -0300

    feat: form conditions in register page

--- src/frontend/src/pages/Register/Register.jsx
+++ src/frontend/src/pages/Register/Register.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import './Register.css';
 import IconFoodPath from '../../public/assets/images/icon-foodpath-white.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faLock, faUser, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faLock, faUser, faEye, faEyeSlash, faCheckCircle, faXmarkCircle } from '@fortawesome/free-solid-svg-icons';
 
 const Register = () => {
   const [password, setPassword] = useState('');
@@ -11,8 +11,16 @@ const Register = () => {
   const [passwordVisible, setPasswordVisible] = useState(false);
   const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
   const [error, setError] = useState('');
-  const [nome, setNome] = useState('');
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
+  const [nameValid, setNameValid] = useState(false);
+  const [passwordValid, setPasswordValid] = useState({
+    length: false,
+    containsLetters: false,
+    containsNumbers: false,
+    containsSpecialChars: false,
+  });
+
   const navigate = useNavigate();
 
   const togglePasswordVisibility = () => {
@@ -23,13 +31,41 @@ const Register = () => {
     setConfirmPasswordVisible(!confirmPasswordVisible);
   };
 
+  const validateName = (inputName) => {
+    setName(inputName);
+    const valid = inputName.trim().split(' ').length > 1;
+    setNameValid(valid);
+  };
+
+  const validatePassword = (inputPassword) => {
+    setPassword(inputPassword);
+    setPasswordValid({
+      length: inputPassword.length >= 8,
+      containsLetters: /[A-Za-z]/.test(inputPassword),
+      containsNumbers: /\d/.test(inputPassword),
+      containsSpecialChars: /[!@#$%^&*(),.?":{}|<>]/.test(inputPassword),
+    });
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (password !== confirmPassword) {
-      setError('As senhas não coincidem.');
+  
+    if (!nameValid) {
+      setError('O nome deve incluir sobrenome.');
       return;
     }
-
+  
+    const isPasswordValid = passwordValid.length && passwordValid.containsLetters && passwordValid.containsNumbers && passwordValid.containsSpecialChars;
+    if (!isPasswordValid) {
+      setError('A senha não cumpre todos os requisitos.');
+      return;
+    }
+  
+    if (password !== confirmPassword) {
+      setError('As senhas devem ser iguais.');
+      return;
+    }
+  
     try {
       const response = await fetch('http://localhost:5000/register', {
         method: 'POST',
@@ -37,24 +73,24 @@ const Register = () => {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          name: nome,
+          name,
           email,
           password,
         }),
       });
-
+  
       const data = await response.json();
       if (response.ok) {
         console.log('Usuário registrado com sucesso', data);
         navigate('/login');
       } else {
-        setError(data.error);
+        setError(data.error || 'Erro ao registrar. Tente novamente.');
       }
     } catch (error) {
       console.error('Erro ao registrar', error);
       setError('Erro ao registrar. Tente novamente.');
     }
-  };
+  };  
 
   return (
     <div className="register-container">
@@ -72,7 +108,7 @@ const Register = () => {
             <input
               type="email"
               className="register-input"
-              placeholder="Exemplo: seuemail@exemplo.com"
+              placeholder="seuemail@exemplo.com"
               required
               value={email}
               onChange={(e) => setEmail(e.target.value)}
@@ -85,11 +121,18 @@ const Register = () => {
             <input
               type="text"
               className="register-input"
-              placeholder="Exemplo: Nome Sobrenome"
+              placeholder="John Doe"
               required
               value={name}
-              onChange={(e) => setName(e.target.value)}
+              onChange={(e) => validateName(e.target.value)}
             />
+          </div>
+
+          <div className="validation-feedback">
+            <p className={nameValid ? 'valid' : 'invalid'}>
+              <FontAwesomeIcon icon={nameValid ? faCheckCircle : faXmarkCircle} />
+              Deve ter o primeiro e o segundo nome.
+            </p>
           </div>
 
           <div className="register-input-group">
@@ -98,10 +141,10 @@ const Register = () => {
             <input
               type={passwordVisible ? 'text' : 'password'}
               className="register-input"
-              placeholder="Digite sua senha"
+              placeholder="Digite sua Senha"
               required
               value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={(e) => validatePassword(e.target.value)}
             />
             <FontAwesomeIcon
               icon={passwordVisible ? faEye : faEyeSlash}
@@ -110,13 +153,32 @@ const Register = () => {
             />
           </div>
 
+          <div className="validation-feedback">
+            <p className={passwordValid.length ? 'valid' : 'invalid'}>
+              <FontAwesomeIcon icon={passwordValid.length ? faCheckCircle : faXmarkCircle} />
+              Deve conter 8 ou mais caracteres.
+            </p>
+            <p className={passwordValid.containsLetters ? 'valid' : 'invalid'}>
+              <FontAwesomeIcon icon={passwordValid.containsLetters ? faCheckCircle : faXmarkCircle} />
+              Deve conter letras.
+            </p>
+            <p className={passwordValid.containsNumbers ? 'valid' : 'invalid'}>
+              <FontAwesomeIcon icon={passwordValid.containsNumbers ? faCheckCircle : faXmarkCircle} />
+              Deve conter números.
+            </p>
+            <p className={passwordValid.containsSpecialChars ? 'valid' : 'invalid'}>
+              <FontAwesomeIcon icon={passwordValid.containsSpecialChars ? faCheckCircle : faXmarkCircle} />
+              Deve conter caracteres especiais.
+            </p>
+          </div>
+
           <div className="register-input-group">
             <FontAwesomeIcon icon={faLock} className="register-input-icon" />
-            <label className="register-label">Confirmar Senha</label>
+            <label className="register-label">Confirme sua Senha</label>
             <input
               type={confirmPasswordVisible ? 'text' : 'password'}
               className="register-input"
-              placeholder="Confirme sua senha"
+              placeholder="Confirme sua Senha"
               required
               value={confirmPassword}
               onChange={(e) => setConfirmPassword(e.target.value)}
@@ -130,10 +192,10 @@ const Register = () => {
 
           {error && <p className="register-error">{error}</p>}
 
-          <button type="submit" className="register-button">Registrar</button>
+          <button type="submit" className="register-button">Criar conta</button>
 
           <p className="register-login-link">
-            Já possui uma conta? <a href="/login">Fazer Login</a>
+            Você já possui uma conta? <a href="/login">Faça Login!</a>
           </p>
         </form>
       </div>

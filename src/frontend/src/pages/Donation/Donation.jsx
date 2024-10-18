ee41c79138a7eeb799dc2c1b05c4ddd1349eeddf
Saulo Pereira Jesus <saulopereiradejesus@gmail.com>
2024-10-18 11:47:44 -0300

    feat: loading and db file

--- src/frontend/src/pages/Donation/Donation.jsx
+++ src/frontend/src/pages/Donation/Donation.jsx
@@ -11,8 +11,12 @@ import acaoCidadaniaImg from '../../public/assets/images/ongs/ong-acao-cidadania
 import sescImg from '../../public/assets/images/ongs/ong-sesc-card.png';
 import caritasBrasilImg from '../../public/assets/images/ongs/ong-caritas-brasileira-card.png';
 
+import loadingGif from '../../public/assets/gifs/loading-gif.gif';
+
 const Donation = () => {
   const [ongs, setOngs] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
   const [cardsRef, cardsInView] = useInView({ triggerOnce: true });
 
   useEffect(() => {
@@ -20,8 +24,11 @@ const Donation = () => {
       try {
         const response = await axios.get('http://localhost:5000/ongs');
         setOngs(response.data);
+        setLoading(false);
       } catch (error) {
         console.error('Erro ao buscar ONGs:', error);
+        setError(true);
+        setLoading(false);
       }
     };
     fetchOngs();
@@ -50,20 +57,32 @@ const Donation = () => {
     <div className="donation-page">
       <h1>Escolha uma ONG para apoiar!</h1>
       <p className="donation-subtitle">
-        Selecione uma das organizações abaixo e contribua diretamente para a luta contra a fome. <br/>
+        Selecione uma das organizações abaixo e contribua diretamente para a luta contra a fome. <br />
         Sua doação pode fazer toda diferença!
       </p>
-      <div ref={cardsRef} className={`donation-cards ${cardsInView ? 'reveal' : ''}`}>
-        {ongs.map(ong => (
-          <Card 
-            key={ong.id}
-            title={ong.name}
-            description={ong.description}
-            link={ong.link}
-            imageSrc={getImageByTitle(ong.name)}
-          />
-        ))}
-      </div>
+
+      {loading ? (
+        <div className="loading">
+          <img src={loadingGif} alt="Loading..." className="loading-gif" />
+          <p>Carregando ONGs...</p>
+        </div>
+      ) : error ? (
+        <div className="error-message">
+          <p>Erro ao carregar as ONGs. Por favor, tente novamente mais tarde.</p>
+        </div>
+      ) : (
+        <div ref={cardsRef} className={`donation-cards ${cardsInView ? 'reveal' : ''}`}>
+          {ongs.map((ong) => (
+            <Card 
+              key={ong.id}
+              title={ong.name}
+              description={ong.description}
+              link={ong.link}
+              imageSrc={getImageByTitle(ong.name)}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 };

6a1f494fa4d751cfe90fe964d189d76c3d988058
Saulo Pereira Jesus <saulopereiradejesus@gmail.com>
2024-11-05 22:14:04 -0300

    fix: try to config vercel

--- src/backend/index.js
+++ src/backend/index.js
@@ -140,7 +140,11 @@ app.get('/profile', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+  });
+}
+
+module.exports = app;
